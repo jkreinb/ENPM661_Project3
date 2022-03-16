0596428a39a9039fa29237a16f1174ba12562ad6
Jonathon Kreinbrink <jkreinb@gmail.com>
2022-03-16 13:55:36 -0400

    initializing search algorithm + helper functions

--- Project3.py
+++ Project3.py
@@ -18,6 +18,11 @@ c2c_node =  np.full((250,400),np.inf)
 # Initializes image file full of pixels with black color (0,0,0) RGB scale
 image = np.zeros((250,400,3),np.uint8)
 
+# Creates window showing obstacle
+flipped_image=cv.flip(image,0)
+cv.imshow("actual_image",flipped_image)
+cv.waitKey(10)
+
 # Creating objects using half planes and semi-algebraic definitions, all object
 # spaces have their cost set to -1
 for x in range(0,400,1):
@@ -52,65 +57,71 @@ goal_set = False
 goal_angle_set = False
 start_set = False
 start_angle_set = False
+global NODEINDEX
+NODEINDEX = 1
 
-# Goal setting loop
-while goal_set == False:
-    print("\nPlease enter a goal location on the workspace\n")
-    print("-------------------------------------------------------\n")
-    goal_x = float(input("Enter X position of goal node, must be an integer between 0-400\n"))
-    goal_y = float(input("Enter Y position of goal node, must be an integer between 0-250\n"))
-    if goal_x < 0 or goal_x > 400 or goal_y > 250 or goal_y < 0: # Checks if goal is outside of workspace
-        print("\nGoal outside of workspace! Please try again\n")
-        time.sleep(2)
-        continue
-    elif c2c_node[goal_y][goal_x] == np.inf:
-        goal_set = True
-    else: # If goal is not equal to np.inf it's an obstacle, prompts user again for new goal
-        print("\nGoal location is ontop of an obstacle! Please enter new goal location\n")
-        time.sleep(2)
-while goal_angle_set == False:
-    goal_theta = int(input("Enter orientation of robot at goal node, must be in degrees between 0-360, in steps of 30 degrees\n"))
-    if (goal_theta > 360) or (goal_theta < 0):
-        print("\n Goal angle out of range, please normalize within 0-360 degrees\n")
-        time.sleep(2)
-        continue
-    elif (goal_theta % 30 != 0):
-        print("\n Goal Angle must be in steps of 30 degrees \n")
-        time.sleep(2)
-    else:
-        goal_angle_set = True
+# # Goal setting loop
+# while goal_set == False:
+#     print("\nPlease enter a goal location on the workspace\n")
+#     print("-------------------------------------------------------\n")
+#     goal_x = float(input("Enter X position of goal node, must be an integer between 0-400\n"))
+#     goal_y = float(input("Enter Y position of goal node, must be an integer between 0-250\n"))
+#     if goal_x < 0 or goal_x > 400 or goal_y > 250 or goal_y < 0: # Checks if goal is outside of workspace
+#         print("\nGoal outside of workspace! Please try again\n")
+#         time.sleep(2)
+#         continue
+#     elif c2c_node[int(goal_y)][int(goal_x)] == np.inf:
+#         goal_set = True
+#     else: # If goal is not equal to np.inf it's an obstacle, prompts user again for new goal
+#         print("\nGoal location is ontop of an obstacle! Please enter new goal location\n")
+#         time.sleep(2)
+# while goal_angle_set == False:
+#     goal_theta = int(input("Enter orientation of robot at goal node, must be in degrees between 0-360, in steps of 30 degrees\n"))
+#     if (goal_theta > 360) or (goal_theta < 0):
+#         print("\n Goal angle out of range, please normalize within 0-360 degrees\n")
+#         time.sleep(2)
+#         continue
+#     elif (goal_theta % 30 != 0):
+#         print("\n Goal Angle must be in steps of 30 degrees \n")
+#         time.sleep(2)
+#     else:
+#         goal_angle_set = True
         
-# Start setting loop
+# # Start setting loop
 
-while start_set == False:
-    print("\nPlease enter a starting location on the workspace\n")
-    print("-------------------------------------------------------\n")
-    start_x = float(input("Enter X position of start node, must be an integer between 0-400\n"))
-    start_y = float(input("Enter Y position of start node, must be an integer between 0-250\n"))
-    if start_x < 0 or start_x > 400 or start_y > 250 or start_y < 0: # Checks if start is outside workspace
-        print("\nStarting location is outside of workspace! Please try again\n")
-        time.sleep(2)
-        continue
-    elif c2c_node[start_y][start_x] == np.inf:
-        start_set = True
-    else: # If start is not equal to np.inf it's an obstacle, prompts user again for new start
-        print("\nStarting location is ontop of an obstacle! Please enter new starting location\n")
-        time.sleep(2)
-while start_angle_set == False:
-    start_theta = int(input("Enter orientation of robot at start node, must be in degrees between 0-360, in steps of 30 degrees\n"))
-    if (start_theta > 360) or (start_theta < 0):
-        print("\n Start angle out of range, please normalize within 0-360 degrees \n")
-        time.sleep(2)
-        continue
-    elif (start_theta % 30 != 0):
-        print("\n Start angle must be in steps of 30 degrees! Please try again! \n")
-        time.sleep(2)
-        continue
-    else:
-        start_angle_set = True
+# while start_set == False:
+#     print("\nPlease enter a starting location on the workspace\n")
+#     print("-------------------------------------------------------\n")
+#     start_x = float(input("Enter X position of start node, must be an integer between 0-400\n"))
+#     start_y = float(input("Enter Y position of start node, must be an integer between 0-250\n"))
+#     if start_x < 0 or start_x > 400 or start_y > 250 or start_y < 0: # Checks if start is outside workspace
+#         print("\nStarting location is outside of workspace! Please try again\n")
+#         time.sleep(2)
+#         continue
+#     elif c2c_node[int(start_y)][int(start_x)] == np.inf:
+#         start_set = True
+#     else: # If start is not equal to np.inf it's an obstacle, prompts user again for new start
+#         print("\nStarting location is ontop of an obstacle! Please enter new starting location\n")
+#         time.sleep(2)
+# while start_angle_set == False:
+#     start_theta = int(input("Enter orientation of robot at start node, must be in degrees between 0-360, in steps of 30 degrees\n"))
+#     if (start_theta > 360) or (start_theta < 0):
+#         print("\n Start angle out of range, please normalize within 0-360 degrees \n")
+#         time.sleep(2)
+#         continue
+#     elif (start_theta % 30 != 0):
+#         print("\n Start angle must be in steps of 30 degrees! Please try again! \n")
+#         time.sleep(2)
+#         continue
+#     else:
+#         start_angle_set = True
 
-goal_node = (goal_y,goal_x,goal_theta)
-start_node = (start_y,start_x,start_theta)
+global goal_node
+#goal_node = (goal_y,goal_x,goal_theta)
+#start_node = (start_y,start_x,start_theta)
+
+goal_node = (25,350,0)
+start_node = (25,25,60)
 
 print(goal_node)
 print(start_node)
@@ -137,6 +148,14 @@ while robot_set == False:
 step_size_set = False
 global STEPSIZE 
 
+# Defining cost calculation function
+
+def Cost_Calc(y,x):
+    dist_y = (goal_node[1]-y)
+    dist_x = (goal_node[0]-x)
+    dist = ((dist_y**2) + (dist_x**2))**.5
+    return dist
+
 # Step size setting loop
 while step_size_set == False:
     print("\nPlease enter desired step size \n")
@@ -148,83 +167,174 @@ while step_size_set == False:
         continue
     else:
         step_size_set = True
-        
+
+## Node Index operates via format [Euclidean Cost, Current Node, Parent Node, Position/Rot]
 def Move0(CurrentNode):
     global NODEINDEX
+    global STEPSIZE
     NODEINDEX = NODEINDEX + 1
-    NewNode = [0,0,0,0,0]
+    NewNode = [0,0,0,0]
     # Parent Node Setting
-    NewNode[0] = CurrentNode[0] + 1
+    NewNode[2] = CurrentNode[1] + 1
     # Global Index Setting
     NewNode[1] = NODEINDEX
-    # Adding Cost (Step Length)
-    NewNode[2] = CurrentNode[1]+STEPSIZE
-    # Adjusting Position
-    NewNode[3] = (CurrentNode[3][0]+(STEPSIZE*np.cos(CurrentNode[4])),CurrentNode[3][1]+(STEPSIZE*np.sin(CurrentNode[4])))    
-    # Adjusting Orientation
-    NewNode[4] = CurrentNode[4]
+    # Generating New Node Pos
+    new_y = CurrentNode[3][0]+(STEPSIZE*np.sin(CurrentNode[3][2]))
+    new_x = CurrentNode[3][1]+(STEPSIZE*np.cos(CurrentNode[3][2]))
+    # Generating New Cost
+    NewNode[0] = Cost_Calc(new_y,new_x)
+    # Adjusting Position/Orientation
+    NewNode[3] = (new_y,new_x,CurrentNode[3][2])
     return NewNode
 
 def MoveP30(CurrentNode):
     global NODEINDEX
+    global STEPSIZE
     NODEINDEX = NODEINDEX + 1
-    NewNode = [0,0,0,0,0]
+    NewNode = [0,0,0,0]
     # Parent Node Setting
-    NewNode[0] = CurrentNode[0] + 1
+    NewNode[2] = CurrentNode[1] + 1
     # Global Index Setting
     NewNode[1] = NODEINDEX
-    # Adding Cost (Step Length)
-    NewNode[2] = CurrentNode[1]+STEPSIZE
+    # Generating New Node Pos
+    new_y = CurrentNode[3][0]+(STEPSIZE*np.sin(CurrentNode[3][2]+30))
+    new_x = CurrentNode[3][1]+(STEPSIZE*np.cos(CurrentNode[3][2]+30))
+    # Generating New Cost
+    NewNode[0] = Cost_Calc(new_y,new_x)
     # Adjusting Position
-    NewNode[3] = (CurrentNode[3][0]+(STEPSIZE*np.cos(CurrentNode[4]+30)),CurrentNode[3][1]+(STEPSIZE*np.sin(CurrentNode[4]+30)))    
-    # Adjusting Orientation
-    NewNode[4] = CurrentNode[4] + 30
+    NewNode[3] = (new_y,new_x,CurrentNode[3][2]+30)
     return NewNode
 
 def MoveP60(CurrentNode):
     global NODEINDEX
     NODEINDEX = NODEINDEX + 1
-    NewNode = [0,0,0,0,0]
+    NewNode = [0,0,0,0]
     # Parent Node Setting
-    NewNode[0] = CurrentNode[0] + 1
+    NewNode[2] = CurrentNode[1] + 1
     # Global Index Setting
     NewNode[1] = NODEINDEX
-    # Adding Cost (Step Length)
-    NewNode[2] = CurrentNode[1]+STEPSIZE
+    # Generating New Node Pos
+    new_y = CurrentNode[3][0]+(STEPSIZE*np.sin(CurrentNode[3][2]+60))
+    new_x = CurrentNode[3][1]+(STEPSIZE*np.cos(CurrentNode[3][2]+60))
+    # Generating New Cost
+    NewNode[0] = Cost_Calc(new_y,new_x)
     # Adjusting Position
-    NewNode[3] = (CurrentNode[3][0]+(STEPSIZE*np.cos(CurrentNode[4]+60)),CurrentNode[3][1]+(STEPSIZE*np.sin(CurrentNode[4]+60)))    
-    # Adjusting Orientation
-    NewNode[4] = CurrentNode[4] + 60
+    NewNode[3] = (new_y,new_x,CurrentNode[3][2]+60)    
     return NewNode 
 
 def MoveN30(CurrentNode):
     global NODEINDEX
+    global STEPSIZE
     NODEINDEX = NODEINDEX + 1
-    NewNode = [0,0,0,0,0]
+    NewNode = [0,0,0,0]
     # Parent Node Setting
-    NewNode[0] = CurrentNode[0] + 1
+    NewNode[2] = CurrentNode[1] + 1
     # Global Index Setting
     NewNode[1] = NODEINDEX
-    # Adding Cost (Step Length)
-    NewNode[2] = CurrentNode[1]+STEPSIZE
-    # Adjusting Position
-    NewNode[3] = (CurrentNode[3][0]+(STEPSIZE*np.cos(CurrentNode[4]-30)),CurrentNode[3][1]+(STEPSIZE*np.sin(CurrentNode[4]-30)))    
+    # Generating New Node Pos
+    new_y = CurrentNode[3][0]+(STEPSIZE*np.sin(CurrentNode[3][2]-30))
+    new_x = CurrentNode[3][1]+(STEPSIZE*np.cos(CurrentNode[3][2]-30))
+    # Generating New Cost
+    NewNode[0] = Cost_Calc(new_y,new_x)
     # Adjusting Orientation
-    NewNode[4] = CurrentNode[4] - 30
+    NewNode[3] = (new_y,new_x,CurrentNode[3][2] - 30)
     return NewNode
 
 def MoveN60(CurrentNode):
     global NODEINDEX
+    global STEPSIZE
     NODEINDEX = NODEINDEX + 1
-    NewNode = [0,0,0,0,0]
+    NewNode = [0,0,0,0]
     # Parent Node Setting
-    NewNode[0] = CurrentNode[0] + 1
+    NewNode[2] = CurrentNode[1] + 1
     # Global Index Setting
     NewNode[1] = NODEINDEX
-    # Adding Cost (Step Length)
-    NewNode[2] = CurrentNode[1]+STEPSIZE
-    # Adjusting Position
-    NewNode[3] = (CurrentNode[3][0]+(STEPSIZE*np.cos(CurrentNode[4]-60)),CurrentNode[3][1]+(STEPSIZE*np.sin(CurrentNode[4]-60)))    
+    # Generating New Node Pos
+    new_y = CurrentNode[3][0]+(STEPSIZE*np.sin(CurrentNode[3][2]-60))
+    new_x = CurrentNode[3][1]+(STEPSIZE*np.cos(CurrentNode[3][2]-60))
+    # Generating New Cost
+    NewNode[0] = Cost_Calc(new_y,new_x)    
     # Adjusting Orientation
-    NewNode[4] = CurrentNode[4] - 60
+    NewNode[3] = (new_y,new_x,CurrentNode[3][2] - 60)
     return NewNode
+
+# Creates window showing obstacle
+flipped_image=cv.flip(image,0)
+cv.imshow("actual_image",flipped_image)
+cv.waitKey(10)
+
+# Updates pixels to white in image for searched nodes 
+def UpdateSearched(node):
+    print(node)
+    flipped_image[250-int(node[3][0])][int(node[3][1])]= [255,255,255]
+
+# Updates pixels to green in image for goal nodes 
+def UpdateGoal(node):
+    flipped_image[250-int(node[3][0])][int(node[3][1])]= [0,255,0]
+    
+# Calls cv.imshow to update image, scales image up 4X
+def UpdateImage():
+        resized = cv.resize(flipped_image,(1600,1000))
+        cv.imshow("image",resized)
+        cv.waitKey(1)
+        
+# Checks if an input node is inside of a list
+def Check_List(new_node,list):
+    for i in range(len(list)):
+        if list[i][3] == new_node[3]:
+            return True
+        else:
+            return False
+        
+# Checks if node is not in obstacle space, has been searched, if lower cost found
+# updates the cost respectively
+def Check_Node(new_node,ClosedList,OpenList,c2c_node):
+    newnode_y = int(new_node[3][0])
+    newnode_x = int(new_node[3][1])
+    if Check_List(new_node,ClosedList) == False and c2c_node[newnode_y][newnode_x] != -1:
+        if (Check_List(new_node,OpenList) == False or Check_List(new_node,OpenList) == None) and c2c_node[newnode_y][newnode_x]==np.inf:
+            c2c_node[newnode_y][newnode_x] = new_node[0]
+            UpdateSearched(new_node)
+            hq.heappush(OpenList,new_node)
+        else:
+            if (new_node[0] < c2c_node[newnode_y][newnode_x]):
+                c2c_node[newnode_y][newnode_x] = new_node[0]
+
+# Algorithm Loop
+
+goal_found = False
+
+def a_star_algo(start_node,goal_node,c2c_node):
+    goal_found = False
+    #Open/Closed List creation
+    OpenList = []
+    ClosedList = []
+    hq.heappush(OpenList,((Cost_Calc(start_node[0],start_node[1])),1,0,start_node))  #Pushes starting node into OpenList
+    iterator = 0
+    while OpenList and goal_found == False:
+        current_node = hq.heappop(OpenList)
+        ClosedList.append(current_node)
+        print("node popped")
+        if current_node[3] == goal_node:
+            goal_found = True
+            print("Goal Found!")
+            #goal_route = Backtrack(ClosedList)
+            #return goal_route
+        else:
+            iterator = iterator + 1
+            new_node0 = Move0(current_node)
+            Check_Node(new_node0,ClosedList,OpenList,c2c_node)
+            new_nodeP30 = MoveP30(current_node)
+            Check_Node(new_nodeP30,ClosedList,OpenList,c2c_node)
+            new_nodeP60 = MoveP60(current_node)
+            Check_Node(new_nodeP60,ClosedList,OpenList,c2c_node)
+            new_nodeN30 = MoveN30(current_node)
+            Check_Node(new_nodeN30,ClosedList,OpenList,c2c_node)
+            new_nodeN60 = MoveN60(current_node)
+            Check_Node(new_nodeN60,ClosedList,OpenList,c2c_node)
+            if iterator % 100 == 0: # Only updates image every 100 nodes for speed
+                UpdateImage()
+    if goal_found == False:
+        print("No Goal Found!")
+
+a_star_algo(start_node, goal_node, c2c_node)
